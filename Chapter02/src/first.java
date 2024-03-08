22f0e4a5a0766a6cdfd1dc9bb09871bbe88e0a4d
김유민 <88136758+kym2207@users.noreply.github.com>
2024-03-08 13:09:53 +0900

    2024/03/08
    Chapter2

---
+++ Chapter02/src/first.java
@@ -0,0 +1,14 @@
+/*
+ * 작성일 : 2024년 3월 8일
+ * 작성자 : 컴공부 202395008 김유민
+ * 설명 : 첫 번째 자바 프로그램
+ */
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.print("김유민입니다.");
+		System.out.println("반갑습니다."+"첫 번째 자바 프로그램입니다.");
+	}
+
+}
